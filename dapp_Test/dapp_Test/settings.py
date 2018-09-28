d2f102288978395e942cfc2b181a05d891fbfd85
nedimitri <nedimitri@gmail.com>
2018-09-28 17:41:14 +0100

    adding runtime.txt

--- dapp_Test/dapp_Test/settings.py
+++ dapp_Test/dapp_Test/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+
 DATA_DIR = os.path.dirname(os.path.dirname(__file__))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -58,14 +59,28 @@ LOGIN_URL = 'auth:authentication'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = ''
-STATIC_ROOT = os.path.join(DATA_DIR, 'static')
+# STATIC_ROOT = os.path.join(DATA_DIR, 'static')
+#
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'dapp_Test', 'static'),
+# )
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'dapp_Test', 'static'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 TEMPLATES = [
     {
